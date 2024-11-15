536447d158ae943eed4041f43a5a00c379c2d904
Matias Baez <matiascaneda3@gmail.com>
2024-11-15 13:12:26 -0300

    Add search button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  site: "https://devblog-astro-6y7xr.kinsta.page"
+  integrations: [tailwind(), mdx(), react()],
+  site: "https://devblog-astro-6y7xr.kinsta.page",
+
+  content: {
+    collections: {
+      blog: {
+        schema: ({ z }) => 
+          z.object({
+            title: z.string(),          // Título del artículo
+            pubDate: z.string(),        // Fecha de publicación
+            intro: z.string(),          // Introducción o resumen
+            tag: z.string(),            // Etiqueta del artículo
+            author: z.string(),         // Autor del artículo
+            image: z.string().optional() // Imagen asociada (opcional)
+          }),
+      },
+    },
+  },
 });
+
